82ef03ad96405c3684486b9d75b7f54a9b7e76ea
화해방 <bcl0206@naver.com>
2022-09-23 22:59:48 +0900

    hoverAnimaton

---
+++ main.js
@@ -0,0 +1,26 @@
+const links = document.querySelectorAll('.container a');
+// console.log(links);
+const bg = document.querySelector('.bg');
+// console.log(bg);
+const showClass = 'bg-show';
+//console.log(back);
+for(const link of links){
+  const img = new Image();
+  img.scr = link.getAttribute('data-bg') //라고도 쓸수 있지만!!
+  // img.src = link.dataset.bg; // 이것도 같은 것임
+
+  link.addEventListener('mouseenter',()=>{
+    bg.style.backgroundImage = `url(${link.dataset.bg})`; // this 난 왜 안돼
+    document.body.classList.add(showClass);
+  }) 
+  link.addEventListener('mouseleave',()=>{
+    document.body.classList.remove(showClass);
+  })
+  // console.log(img);
+}
+
+
+// document.addEventListener('click',(ev) => {
+//   let word = ev.target;
+//   console.log(word);
+// })
